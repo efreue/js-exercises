c9c8cd045ff3f6ff68381f3e24d9e51d5ba75bad
efreue <efreue@gmail.com>
2016-08-10 23:47:18 -0300

    Se optimizo el codigo segun observaciones realizadas por charly

--- src/ejercicio-4/main.js
+++ src/ejercicio-4/main.js
@@ -1,99 +1,94 @@
-var getNodesMatchingCss = function(className) {
-    var storeCssNode = [];
-    var matchingNodes = [];
-    var nodes = document.body.getElementsByTagName('*');
-    for (var i = 0; i < nodes.length; i++){
-        storeCssNode = nodes[i].className.split(' ');
-        for(var j = 0; j < storeCssNode.length; j++) {
-            if (storeCssNode[j] === className) {
-                matchingNodes.push(nodes[i]);
-            }
-            break;
-        }
-    }
-    return matchingNodes;
-};
-
 //funcion que asigna los comportamientos que deben tener los nodos
-var addBehaviorNodes = function (nodes) {
-    for (var i = 0; i < nodes.length; i++) {
-        nodes[i].zIndex = "-1";
-        nodes[i].onclick = function() {
-            if(validCssNode(this, "positionable")) {
-                if(!validCssNode(this, "selected")) {
-                    addCssNode(this, "selected");
-                }
-                else {
-                    delCssNode(this, "selected");
-                }
+var addBehaviorNodes = function() {
+    var nodes = document.body.getElementsByClassName('selectable');
+    if (nodes) {
+        for (var i = 0; i < nodes.length; i++) {
+            nodes[i].zIndex = "-1";
+            if (Css.contains(nodes[i], "positionable")) {
+                nodes[i].onclick = function() {
+                    if(Css.contains(this, "positionable")) {
+                        if(!Css.contains(this, "selected")) {
+                            Css.add(this, "selected");
+                        }
+                        else {
+                            Css.del(this, "selected");
+                        }
+                    }
+                };
+                nodes[i].onmouseout = function() {
+                    if(Css.contains(this, "selected")) {
+                        if(!Css.contains(this, "positioned")) {
+                            Css.add(this, "positioned");
+                        }
+                    }
+                };
+                nodes[i].ondblclick = function() {
+                    restorePosition(this);
+                };
             }
-        };
-        nodes[i].onmouseout = function() {
-            if(validCssNode(this, "selected")) {
-                if(!validCssNode(this, "positioned")) {
-                    addCssNode(this, "positioned");
-                }
-            }
-        };
-        nodes[i].ondblclick = function() {
-            moveNodePositionOrigin(this);
-        };
+        }
     }
 };
 
 var moveElementPositioned = function(e) {
     var evento = e || window.event;
     var node = document.body.getElementsByClassName("selected")[0];
-    if (node != undefined) {
-        switch (evento.type) {
-            case 'mousemove':
-                var ie = navigator.userAgent.toLowerCase().indexOf('msie') != -1;
-                var coordenadaXabsoluta, coordenadaYabsoluta;
-                if (ie) {
-                    if (document.documentElement && document.documentElement.scrollTop) {
-                        coordenadaXabsoluta = evento.clientX + document.documentElement.scrollLeft;
-                        coordenadaYabsoluta = evento.clientY + document.documentElement.scrollTop;
-                    }
-                    else {
-                        coordenadaXabsoluta = evento.clientX + document.body.scrollLeft;
-                        coordenadaYabsoluta = evento.clientY + document.body.scrollTop;
-                    }
-                }
-                else {
-                    coordenadaXabsoluta = evento.pageX;
-                    coordenadaYabsoluta = evento.pageY;
-                }
-                node.style.left = (coordenadaXabsoluta - node.parentNode.getBoundingClientRect().left) - 30;
-                node.style.top = (coordenadaYabsoluta - node.parentNode.getBoundingClientRect().top) - 30;
-                break;
+    if (node) {
+        if (evento.type === 'mousemove') {
+			var getNewMouseCoordinates = getMouseCoordinates(evento);
+			setPosition(node, getNewMouseCoordinates);
+	    }
+    }
+};
+
+
+var getMouseCoordinates = function(evento) {
+    var ie = navigator.userAgent.toLowerCase().indexOf('msie') != -1;
+    var coordinateXabsolute, coordinateYabsolute;
+    if (ie) {
+        if (document.documentElement && document.documentElement.scrollTop) {
+            coordinateXabsolute = evento.clientX + document.documentElement.scrollLeft;
+            coordinateYabsolute = evento.clientY + document.documentElement.scrollTop;
         }
+        else {
+            coordinateXabsolute = evento.clientX + document.body.scrollLeft;
+            coordinateYabsolute = evento.clientY + document.body.scrollTop;
+       }
+    }
+    else {
+       coordinateXabsolute = evento.pageX;
+       coordinateYabsolute = evento.pageY;
+    }
+	return {
+		posX: coordinateXabsolute,
+		posY: coordinateYabsolute
+	};
+};
+
+var setPosition = function(node, MouseCoordinates) {
+	node.style.left = (MouseCoordinates.posX - node.parentNode.getBoundingClientRect().left) - 30;
+	node.style.top = (MouseCoordinates.posY - node.parentNode.getBoundingClientRect().top) - 30;
+};
+
+var restorePosition = function(node) {
+    if (Css.contains(node, "positioned")) {
+       Css.del(node, "positioned");
+       Css.del(node, "selected");
     }
 };
 
-var moveNodePositionOrigin = function(node) {
-    if (validCssNode(node, "positioned")) {
-       delCssNode(node, "positioned");
-       delCssNode(node, "selected");
-    }
+//creo objeto Css que agrupa funciones
+var Css = {
+	add: function (node, className) {
+    	node.className += " " + className;
+	},
+	del: function (node, className) {
+    	node.className = node.className.replace(className, "");
+	},
+	contains: function (node, className) {
+    	return node.className.search(className) != -1;
+	}
 };
 
-//agregar clase al nodo
-var addCssNode = function (node, className) {
-    node.className += " " + className;
-};
-
-//borrar clase del nodo
-var delCssNode = function (node, className) {
-    node.className = node.className.replace(className, "");
-};
-
-//valido si existe o no la clase en el nodo
-var validCssNode = function (node, className) {
-    return node.className.search(className) != -1;
-};
-
-window.onload = function() {
-    var nodes = getNodesMatchingCss("selectable");
-    addBehaviorNodes(nodes);
-};
+window.onload = addBehaviorNodes;
 document.onmousemove = moveElementPositioned;
