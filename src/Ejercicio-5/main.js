0ab52d39fdc3d2fd7c0feacb7a03d65015006b62
efreue <efreue@gmail.com>
2016-08-15 18:54:02 -0300

    Se modifico el ejercicio 5 para hacerlo con objetos prototype

--- src/Ejercicio-5/main.js
+++ src/Ejercicio-5/main.js
@@ -1,38 +1,47 @@
-var addNewBall = function() {
-    var second = 24;
-	var MaxPositionLeft = 800;
-	var newBall = document.createElement("div");
-    Css.add(newBall, "ball");
-    document.body.appendChild(newBall);
-	PlayBall(newBall, second, MaxPositionLeft);
-};
-
 var Css = {
 	add: function (node, className) {
     	node.className += " " + className;
 	}
 };
 
-var PlayBall = function(newBall, second, MaxPositionLeft) {
-	var IdInterval = setInterval(MoveBallLeft, 80);
-	function MoveBallLeft() {
-		startMoveBall(newBall, second, MaxPositionLeft);
-	}
+var Config = {
+	maxXPosition: 800,
+	step: 24
+};
 
-	var startMoveBall = function(newBall, second, MaxPositionLeft) {
-		var LeftPos = newBall.getBoundingClientRect().left;
-		if (LeftPos < MaxPositionLeft)
-		{
-			newBall.style.left = (LeftPos + second) + 'px';
-		}
-		else
-		{
-			newBall.style.left = MaxPositionLeft + 'px';
-			StopMoveBall(IdInterval);
-		}
-	}
+var EventBall = function(newBall) {};
 
-	var StopMoveBall = function (IdInterval) {
-		clearInterval(IdInterval);
+EventBall.prototype.getPosition = function(newBall) {
+	return newBall.getBoundingClientRect().left;
+};
+
+EventBall.prototype.stopMoveLeft = function(idInterval) {
+	clearInterval(idInterval);
+};
+
+EventBall.prototype.startmoveLeft = function(newBall,idInterval) {
+	var posMove = parseInt(newBall.style.left);
+	if (( posMove + Config.step) < Config.maxXPosition) {
+			newBall.style.left = posMove + Config.step + 'px';
+	}
+	else {
+		this.StopMoveLeft(idInterval);
 	}
 };
+
+
+var PlayBall = function(newBall) {
+	var myBall = new EventBall(newBall);
+	var	leftPos = myBall.getPosition(newBall)
+	newBall.style.left = leftPos;
+	var idInterval = setInterval(myBall.startmoveLeft, 80, newBall, idInterval);
+};
+
+
+var addNewBall = function() {
+    var newBall = document.createElement("div");
+    Css.add(newBall, "ball");
+    document.body.appendChild(newBall);
+	PlayBall(newBall);
+};
+
