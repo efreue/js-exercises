15a1528453bdc53238e7689ca521693934638a1c
efreue <efreue@gmail.com>
2016-08-18 01:43:06 -0300

    Se finalizo el ejercicio 5

--- src/Ejercicio-5/main.js
+++ src/Ejercicio-5/main.js
@@ -16,45 +16,88 @@ var Ball = function(initLeft, initTop, element) {
 	this.element = element;
 	this.x = initLeft;
 	this.y = initTop;
-	this.interval = 0;
-	this.finish = 0;
 };
 
-Ball.prototype.setInterval = function(idIntervalBall) {
-	this.interval = idIntervalBall;
-}
-
 Ball.prototype.updateLeft = function() {
-	var x = this.x + Config.step;
-	if(x < Config.maxXPosition) {
-		this.x = x;
-		this.element.style.left = this.x + 'px';
+	if (App.interval !== null){
+		document.getElementById("stop").disabled = false;
+	}
+	if (App.paused == 1) {
+			return;
+	}
+	else {
+		for(var i = 0; i< App.ball.length; i++) {
+			var x = App.ball[i].x + Config.step;
+			if(x < Config.maxXPosition) {
+				App.ball[i].x = x;
+				App.ball[i].element.style.left = App.ball[i].x + 'px';
+			}
+		}
 	}
 };
 
 var App = {
-	ball: null,
+	paused: 0,
+	cont: 0,
+	ball:[],
 	interval: null,
 	onEnterFrame: function() {
-		if (ball.interval == 0) {
-			ball.setInterval(App.terval);
-		}
-		ball.updateLeft();
-		if (ball.finish == 1) {
-			App.stop();
+		if (App.ball[App.cont -1]) {
+			App.ball[App.cont - 1].updateLeft();
 		}
 	},
 	start: function() {
-		App.terval = setInterval(App.onEnterFrame, Config.interval);
+		App.interval = setInterval(App.onEnterFrame, Config.interval);
 	},
 	stop: function() {
-		clearInterval(ball.interval);
+		if (App.interval) {
+			clearInterval(App.interval);
+			App.interval = null;
+			App.ActiveButton("stop");
+		}
+	},
+	pause: function() {
+		App.paused = 1;
+		App.ActiveButton("pause");
+	},
+	reStart: function() {
+		App.paused = 0;
+		App.ActiveButton("reStart");
+	},
+	ActiveButton: function(idButton) {
+		switch(idButton) {
+			case "stop":
+				document.getElementById("pause").disabled = true;
+				document.getElementById("reStart").disabled = true;
+				break;
+			case "pause":
+				document.getElementById("pause").disabled = true;
+				document.getElementById("reStart").disabled = false;
+				break;
+			case "reStart":
+				document.getElementById("pause").disabled = false;
+				document.getElementById("reStart").disabled = true;
+				break;
+			case "add":
+				document.getElementById("pause").disabled = false;
+				document.getElementById("reStart").disabled = true;
+				break;
+		}
+
+
 	},
 	createBall: function() {
 		var element = document.createElement("div");
 		Css.add(element, "ball");
 		document.body.appendChild(element);
-		ball = new Ball(Config.initLeft, Config.initTop, element);
-		App.start();
+		App.ball[App.cont] = new Ball(Config.initLeft, Config.initTop, element);
+		App.cont += 1;
+		if (App.paused == 1) {
+			App.reStart();
+		}
+		if (App.interval === null) {
+			App.start();
+		}
+		App.ActiveButton("add");
 	}
 };
