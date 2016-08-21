491af9479dc1ce647a7164923160139e6cdd553d
efreue <efreue@gmail.com>
2016-08-20 23:22:04 -0300

    Se genero una nueva version del ejercicio 5

--- src/Ejercicio-5/main.js
+++ src/Ejercicio-5/main.js
@@ -11,7 +11,7 @@ var Config = {
 	initLeft: 100,
 	initTop: 100,
 	increaseTop: 90,
-	countBall: 0,
+	firstClickButtonAdd: 0,
 	acumulateTop: function () {
 		return Config.initTop += Config.increaseTop;
 	}
@@ -21,34 +21,73 @@ var Ball = function(initLeft, initTop, element) {
 	this.element = element;
 	this.x = initLeft;
 	this.y = initTop;
-	this.moveLeft = function() {
-		var x = this.x + Config.step;
-		if(x < Config.maxXPosition) {
-			this.x = x;
-			this.element.style.left = this.x + 'px';
-		}
-	};
-	this.moveTop = function() {
+	this.positionDown = function() {
 		this.element.style.top = this.y + 'px';
-	}
+	};
 };
 
 var App = {
+	paused: 0,
+	interval: null,
 	createBall: function() {
 		var setTopBall = 0;
 		var element = document.createElement("div");
+		element.className = "clsBall";
 		Css.add(element, "ball");
 		document.body.appendChild(element);
-		if (Config.countBall == 0) {
+		if (Config.firstClickButtonAdd == 0) {
 			setTopBall = Config.initTop;
 		}
 		else {
 			setTopBall = Config.acumulateTop();
 		}
-		Config.countBall+=1;
 		var ball = new Ball(Config.initLeft, setTopBall, element);
-
-		ball.moveTop();
-		ball.moveLeft();
+		ball.positionDown();
+		if (Config.firstClickButtonAdd == 0) {
+			App.activeButton("add");
+			App.start();
+			Config.firstClickButtonAdd==1;
+		}
+	},
+	updateLeft: function() {
+		if (App.paused == 1) {
+			return;
+		}
+		else {
+			var nodes = document.getElementsByClassName("clsBall");
+			for (var i=0; i< nodes.length; i++) {
+				var x = nodes[i].offsetLeft	+ Config.step;
+				if(x < Config.maxXPosition) {
+					nodes[i].style.left = x + 'px';
+				}
+			}
+		}
+	},
+	start: function() {
+		App.interval = setInterval(App.updateLeft, Config.interval);
+	},
+	pause: function() {
+		App.paused = 1;
+		App.activeButton("pause");
+	},
+	play: function() {
+		App.paused = 0;
+		App.activeButton("play");
+	},
+	activeButton: function(idButton) {
+		switch(idButton) {
+			case "pause":
+				document.getElementById("pause").disabled = true;
+				document.getElementById("play").disabled = false;
+				break;
+			case "play":
+				document.getElementById("pause").disabled = false;
+				document.getElementById("play").disabled = true;
+				break;
+			case "add":
+				document.getElementById("pause").disabled = false;
+				document.getElementById("play").disabled = true;
+				break;
+		}
 	}
 }
