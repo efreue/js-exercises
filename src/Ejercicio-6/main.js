4c257052c023cf93185364304a366a6776aa947a
efreue <efreue@gmail.com>
2016-08-25 08:23:45 -0300

    Se comenzo el ejercicio 6. Se consiguio movimiento en diagonal

--- src/Ejercicio-6/main.js
+++ src/Ejercicio-6/main.js
@@ -1,72 +1,92 @@
 var Css = {
 	add: function(node, className) {
-    	node.className += " " + className;
+		node.className += " " + className;
 	}
 };
 
 var Config = {
-	maxXPosition: window.innerWidth,
-	maxYPosition: window.innerHeight,
 	step: 24,
-	direction: "left"
-};
-
-var EventBall = function(newBall) {};
-
-EventBall.prototype.getPositionLeft = function(newBall) {
-	return newBall.getBoundingClientRect().left;
-};
-
-EventBall.prototype.getPositionTop = function(newBall) {
-	return newBall.getBoundingClientRect().top;
-};
-
-EventBall.prototype.stopMove = function(idInterval) {
-	clearInterval(idInterval);
-};
-
-EventBall.prototype.startMove = function(newBall, myBall) {
-	var idInterval = setInterval(myBall.startMove, 80, newBall, myBall);
-	var posMoveLeft = parseInt(newBall.style.left);
-	var posMoveTop = parseInt(newBall.style.top);
-	var directionBall = Config.direction;
-	switch (directionBall) {
-		case "left":
-			if ((posMoveLeft + Config.step) < Config.maxXPosition) {
-				newBall.style.left = posMoveLeft + Config.step + 'px';
-			}
-			else {
-				myBall.stopMove(idInterval);
-				Config.direction = "diagonalRight"
-			}
-			break;
-		case "diagonalRight":
-			if (((posMoveLeft - Config.step) > 0) && ((posMoveTop - Config.step) > 0)) {
-				newBall.style.left = posMoveLeft - Config.step + 'px';
-				newBall.style.top = posMoveTop - Config.step + 'px';
-			}
-			else {
-				myBall.stopMove(idInterval);
-				Config.direction = "chau"
-			}
-			break;
-		defaul:
-			break;
+	interval: 80,
+	initLeft: 100,
+	initTop: 100,
+	increaseTop: 90,
+	firstClickButtonAdd: 0,
+	acumulateTop: function () {
+		return Config.initTop += Config.increaseTop;
 	}
-
 };
 
-var PlayBall = function(newBall) {
-	var myBall = new EventBall(newBall);
-	newBall.style.left = myBall.getPositionLeft(newBall);
-	newBall.style.top = myBall.getPositionTop(newBall);
-	myBall.startMove(newBall,myBall);
+var Ball = function(initLeft, initTop, element) {
+	this.element = element;
+	this.x = initLeft;
+	this.y = initTop;
+	this.move = function() {
+		if (App.paused == 1) {
+			return;
+		}
+		else {
+			var x = this.element.offsetLeft	+ Config.step;
+			var y = this.element.offsetTop	+ Config.step;
+			if(x <= document.body.clientWidth) {
+				this.element.style.left = x + 'px';
+			}
+			if(y <= document.body.clientHeight) {
+				this.element.style.top = y + 'px';
+			}
+		}
+	}
 };
 
-var addNewBall = function() {
-    var newBall = document.createElement("div");
-    Css.add(newBall, "ball");
-    document.body.appendChild(newBall);
-	PlayBall(newBall);
-};
-
+var App = {
+	paused: 0,
+	interval: null,
+	createBall: function() {
+		var setTopBall = 0;
+		var element = document.createElement("div");
+		element.className = "clsBall";
+		Css.add(element, "ball");
+		document.body.appendChild(element);
+		if (Config.firstClickButtonAdd == 0) {
+			setTopBall = Config.initTop;
+		}
+		else {
+			setTopBall = Config.acumulateTop();
+		}
+		var ball = new Ball(Config.initLeft, setTopBall, element);
+		if (Config.firstClickButtonAdd == 0) {
+			App.activeButton("add");
+			App.start(ball);
+			Config.firstClickButtonAdd = 1;
+		}
+	},
+	moveObj: function(ball) {
+		ball.move();
+	},
+	start: function(ball) {
+		App.interval = setInterval(App.moveObj, Config.interval, ball);
+	},
+	pause: function() {
+		App.paused = 1;
+		App.activeButton("pause");
+	},
+	play: function() {
+		App.paused = 0;
+		App.activeButton("play");
+	},
+	activeButton: function(idButton) {
+		switch(idButton) {
+			case "pause":
+				document.getElementById("pause").disabled = true;
+				document.getElementById("play").disabled = false;
+				break;
+			case "play":
+				document.getElementById("pause").disabled = false;
+				document.getElementById("play").disabled = true;
+				break;
+			case "add":
+				document.getElementById("pause").disabled = false;
+				document.getElementById("play").disabled = true;
+				break;
+		}
+	}
+}
