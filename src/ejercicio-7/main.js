7c410e74b4ff61779d6e667080050b72c8093280
efreue <efreue@gmail.com>
2016-09-02 01:06:09 -0300

    Se obtuvo datos de json y se mostro html

---
+++ src/ejercicio-7/main.js
@@ -0,0 +1,28 @@
+var readDataJson = function() {
+	var xmlhttp = new XMLHttpRequest(); // Creo el objeto XMLHttpRequest
+	var url = "http://127.0.0.1:50986/data.json";
+	xmlhttp.onload = function() {
+		if(this.status == 200) {
+			//cargo en un array el contenido del archivo json
+			var arrDataJson = JSON.parse(this.responseText);
+			generateHTML(arrDataJson);
+		}
+	};
+	//se prepara la peticion al servidor
+	xmlhttp.open("GET", url, true);
+	//se envia peticion
+	xmlhttp.send();
+};
+
+var generateHTML = function(element) {
+	var newContent = "";
+	//recorro el contenido del json
+	for (var i = 0; i < element.length; i++) {
+		newContent += '<div class="container">';
+		newContent += '<p class="center">'+element[i].title+'</p><br>';
+		newContent += '<img src="'+ element[i].url +'" class="icon"><br>';
+		newContent += '</div">';
+	}
+	//Actualizando la pagina con el nuevo contenido
+	document.getElementById('content').innerHTML = newContent;
+};
