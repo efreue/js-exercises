6d57a17bf2530ee11a6586b2f40cdff1e7b16394
efreue <efreue@gmail.com>
2016-09-22 00:13:01 -0300

    Se esta siguiendo la logica segun lo planificado

--- src/Ejercicio-8/main.js
+++ src/Ejercicio-8/main.js
@@ -1,48 +1,87 @@
 var Config = {
 	path: "./data/",
-	idElementDom: ["listJson", "containJson","imgJson"],
-	loadedButtons: false
-};
-var getListUrls = function() {
-	var listUrls=[];
-	var url = "";
-	for(i=1; i <= 30; i++) {
-		url = "data-" + i + ".json";
-		listUrls.push(url);
-	}
-	return listUrls;
+	fileNameSelected: "",
+	divIdSelected: ""
 };
 
-var generateHTML = function(atributeShow, dataJS) {
-	var newContent = "";
-	if (atributeShow === "files") {
-		for (i=0; i < dataJS.length; i++) {
-			newContent += '<button class="roundButton"';
-			newContent += 'onclick="obtainDataSelect('+dataJS[i]+','+Config.idElementDom[0]+')">';
-			newContent += dataJS[i]+'</button>';
+var setFileNameJsSelected = function(fileNameJs) {
+	Config.fileNameSelected = fileNameJs;
+};
+
+var setDivIdSelected = function(divId) {
+	Config.divIdSelected = divId;
+};
+
+var getListNameFileJS = function() {
+	var listFile=[];
+	var file = "";
+	for(i=1; i <= 30; i++) {
+		file = "data-" + i + ".json";
+		listFile.push(file);
+	}
+	return listFile;
+};
+
+var getFirstNameFileJS = function(getListNameFileJS) {
+	var listFile = getListNameFileJS();
+	var fileName = "";
+	if(listFile.length > 0){
+		fileName = listFile[1];
+	}
+	return listFile;
+};
+
+var getURL = function(fileName, path) {
+	var url = path + fileName;
+    return url;
+};
+
+var getDataJS = function(fileName, divId, text) {
+	var arrDataJson = JSON.parse(text);
+	if (arrDataJson.length > 0)
+	{
+		console.log(arrDataJson["1"].title);
+		console.log(fileName);
+		console.log(divId);
+		//getHTMLDataJS(arrDataJson);
+	}
+};
+
+var generateHttpRequest = function(fileName, divId, path, getURL, getDataJS) {
+	var url = getURL(fileName, path);
+	var objs = new XMLHttpRequest();
+	objs.onload = function() {
+		if(this.status == 200) {
+			getDataJS(fileName, divId, this.responseText);
 		}
+	};
+	objs.open("GET", url, true);
+	objs.send();
+};
+
+var showHTML = function(textHTML){
+	if(Config.divIdSelected ==="listJson") {
+		document.getElementById("listJson").innerHTML = textHTML;
+	}
+}
+
+var getHTMLListFileNameJS = function(getListNameFileJS) {
+	var newContent = "";
+	var listFiles = getListNameFileJS();
+	for (i=0; i < listFiles.length; i++) {
+		newContent += '<button class="roundButton"';
+		newContent += 'onclick=generateHttpRequest("'+listFiles[i]+'",';
+		newContent += '"listJson","'+Config.path+'",getURL,getDataJS)>';
+		newContent += listFiles[i]+'</button>';
 	}
 	return newContent;
 };
 
-var showHTML = function(idElement, txtHtml) {
-	document.getElementById(idElement).innerHTML = txtHtml;
-}
-
-var obtainDataSelect = function(btnName, idDiv){
-	console.log(btnName);
-	console.log(idDiv);
-};
-
 var App = {
 	init: function() {
-		var listUrl = getListUrls();
-		var textHTML = generateHTML("files", listUrl);
-		showHTML(Config.idElementDom[0], textHTML);
+		var HTMLText = getHTMLListFileNameJS(getListNameFileJS);
+		setFileNameJsSelected(getFirstNameFileJS);
+		setDivIdSelected("listJson");
+		showHTML(HTMLText);
 	}
-};
-/*
-listar_botones
-obtener_data_boton_seleccionado
-mostrar_data_boton_seleccionado
-*/
+}
