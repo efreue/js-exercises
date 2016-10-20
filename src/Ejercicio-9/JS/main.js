305beaaef1b1dd158288028334587403886e30bc
efreue <efreue@gmail.com>
2016-10-19 22:31:26 -0300

    se incorporo el evento DOMContentLoaded para poder referenciar elementos html despues de que estos se carguen

--- src/Ejercicio-9/JS/main.js
+++ src/Ejercicio-9/JS/main.js
@@ -1,11 +1,24 @@
 //TEMPLATE PERSONS
-var source = document.getElementById("personsTemplate").innerHTML;
-var template = Handlebars.compile(source);
+var generateTemplate = function() {
+    var source = document.getElementById("personsTemplate").innerHTML;
+    var template = Handlebars.compile(source);
+    var data = {persons: [
+        {firstName: "Miguel", lastName: "Rojas", email: "mrojas@gmail.com"},
+        {firstName: "Juan", lastName: "Perez", email: "jperez@yahoo.com.ar"}
+    ]};
+    var output = template(data);
+    document.getElementById("personList").innerHTML += output;
+};
+//Se utiliza el evento DOMContentLoaded para esperar a que se cargue el html antes de hacer referencia a los mismos
+if (document.addEventListener) {
+    document.addEventListener( "DOMContentLoaded", function(){
+         generateTemplate();
+    }, false );
+} else if ( document.attachEvent ) { // IE
+    document.attachEvent("onreadystatechange", function(){
+        if ( document.readyState === "complete" ) {
+            generateTemplate();
+        }
+    });
+}
 
-var data = {persons: [
-    {firstName: "Miguel", lastName: "Rojas", email: "mrojas@gmail.com"},
-    {firstName: "Juan", lastName: "Perez", email: "jperez@yahoo.com.ar"}
-]};
-
-var output = template(data);
-document.getElementById("personList").innerHTML += output;
