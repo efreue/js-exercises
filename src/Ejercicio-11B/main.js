7c6d4a88a9ad65f551d6d5e5b960981402348145
efreue <efreue@gmail.com>
2016-12-15 00:58:13 -0300

    example con table que no funciona

--- src/Ejercicio-11B/main.js
+++ src/Ejercicio-11B/main.js
@@ -1,4 +1,4 @@
-var ViewModel = function(first, last) {
+var ViewModelFullName = function(first, last) {
     this.firstName = ko.observable(first);
     this.lastName = ko.observable(last);
 
@@ -6,5 +6,23 @@ var ViewModel = function(first, last) {
         return this.firstName() + " " + this.lastName();
     }, this);
 };
+ko.applyBindings(new ViewModelFullName("Emiliano", "Freue"));
 
-ko.applyBindings(new ViewModel("Emiliano", "Freue"));
+var initialData = [
+    { name: "Well-Travelled Kitten", sales: 352, price: 75.95 },
+    { name: "Speedy Coyote", sales: 89, price: 190.00 },
+    { name: "Furious Lizard", sales: 152, price: 25.00 },
+    { name: "Indifferent Monkey", sales: 1, price: 99.95 },
+    { name: "Brooding Dragon", sales: 0, price: 6350 },
+    { name: "Ingenious Tadpole", sales: 39450, price: 0.35 },
+    { name: "Optimistic Snail", sales: 420, price: 1.50 }
+];
+
+var PagedGridModel = function(items) {
+    this.items = ko.observableArray(items);
+};
+
+ko.applyBindings(new PagedGridModel(initialData));
+
+
+
