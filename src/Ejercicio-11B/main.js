8d874616923a314f8812df81f164ea188f792ddb
efreue <efreue@gmail.com>
2016-12-15 23:53:11 -0300

    no anda

--- src/Ejercicio-11B/main.js
+++ src/Ejercicio-11B/main.js
@@ -8,15 +8,10 @@ var ViewModelFullName = function(first, last) {
 };
 ko.applyBindings(new ViewModelFullName("Emiliano", "Freue"));
 
-var viewModelTable = {
-    stringValue : ko.observable("Hello"),
-    passwordValue : ko.observable("mypass"),
-    booleanValue : ko.observable(true),
-    optionValues : ["Alpha", "Beta", "Gamma"],
-    selectedOptionValue : ko.observable("Gamma"),
-    multipleSelectedOptionValues : ko.observable(["Alpha"]),
-    radioSelectedOptionValue : ko.observable("Beta")
-};
-ko.applyBindings(viewModelTable);
-
-
+ko.applyBindings({
+        people: [
+            { firstName: 'Bert', lastName: 'Bertington' },
+            { firstName: 'Charles', lastName: 'Charlesforth' },
+            { firstName: 'Denise', lastName: 'Dentiste' }
+        ]
+    });
