bd2594f5ed7b675d2f8e12509a42d575fbe4d118
efreue <efreue@gmail.com>
2016-12-20 00:16:29 -0300

    se corrigieron los errores sugeridos por charly, pero todavia no logro cargar la tabla

--- src/Ejercicio-11B/main.js
+++ src/Ejercicio-11B/main.js
@@ -1,17 +1,31 @@
-var ViewModelFullName = function(first, last) {
+var viewModel = function(first, last, people) {
     this.firstName = ko.observable(first);
     this.lastName = ko.observable(last);
 
     this.fullName = ko.computed(function() {
         return this.firstName() + " " + this.lastName();
     }, this);
-};
-ko.applyBindings(new ViewModelFullName("Emiliano", "Freue"));
 
-ko.applyBindings({
-        people: [
-            { firstName: 'Bert', lastName: 'Bertington' },
-            { firstName: 'Charles', lastName: 'Charlesforth' },
-            { firstName: 'Denise', lastName: 'Dentiste' }
-        ]
-    });
+    this.people = ko.observable(people);
+};
+
+//se define que se ejecuta cuando se carga la pagina
+var init = function() {
+    var nodes = document.body.getElementsByClassName('FullNameExample');
+    if(nodes) {
+        ko.applyBindings(new viewModel(
+            "Emiliano",
+            "Freue",
+            {
+                people: [
+                    { firstName: 'Bert', lastName: 'Bertington' },
+                    { firstName: 'Charles', lastName: 'Charlesforth' },
+                    { firstName: 'Denise', lastName: 'Dentiste' }
+                ]
+            }));
+    }
+};
+
+window.onload = init;
+
+
