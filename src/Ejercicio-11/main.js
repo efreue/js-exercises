801954cecbe5fa87726f207d8ddee92c8cfb15ae
Emiliano Freue <efreue@admotion.local>
2016-12-27 19:46:54 -0300

    ejemplo con charly

--- src/Ejercicio-11/main.js
+++ src/Ejercicio-11/main.js
@@ -9,114 +9,28 @@ var httpRequest = function(url, callback) {
 	ajax.send();
 };
 
-var Css = {
-	add: function(node, className) {
-		node.className += " " + className;
-	},
-	del: function(node, className) {
-		node.className = node.className.replace(className, "");
-	},
-	contains: function(node, className) {
-		return node.className.search(className) != -1;
-	}
-};
-
-var selectColumnTitle = function(divId, selectedColumnId) {
-    var className =  "hidden-column";
-    var listColumn = document.getElementById(divId).getElementsByClassName(className);
-    var lastSelectedColumn = document.getElementById(divId).getElementsByClassName(className)[0];
-    if (lastSelectedColumn && Css.contains(lastSelectedColumn, className)) {
-        Css.del(lastSelectedColumn, className);
-    }
-    for(var i = 0; i < listColumn.length; i++) {
-        if(listColumn[i].id === selectedColumnId) {
-            Css.del(listColumn[i], className);
-        }
-        else {
-            Css.add(listColumn[i], className);
-        }
-    }
-};
-
 var viewModel = function(listCars) {
     this.cars = ko.observableArray(listCars);
+    this.sortTableByColumn1 = function() {
+        this.cars.reverse();
+    };
+    this.sortTableByColumn2 = function() {
+        console.log('sort 2');
+    };
 };
 
-var View = function(mainDivId) {
-    var element = document.getElementById(mainDivId);
-    this.showData = function(dataSorted, selectedColumnTitle) {
-        httpRequest(
-            "https://gist.githubusercontent.com/z4y4ts/7170953/raw/7a2b09105b69de8673c4c3acd2b256b83a171dcf/cars.json",
-            function(data) {
-                ko.applyBindings(new viewModel(dataSorted));
-                var selectedColumnId = Views.getColumnId(mainDivId, selectedColumnTitle);
-            }
-        )
-    }
-};
-
-var Views = {
-    dataView: null,
-    createViews: function() {
-        Views.dataView = new View('listCars');
-    },
-    sortBy: function(data, nameColumnSelected) {
-       return data.sort(
-            function(a, b) {
-                if(a[nameColumnSelected].toUpperCase() < b[nameColumnSelected].toUpperCase()) {
-                    return -1;
-                }
-                if(a[nameColumnSelected].toUpperCase() > b[nameColumnSelected].toUpperCase()) {
-                    return 1;
-                }
-                return 0;
-            }
-        );
-    },
-    getSelectedColumn: function(selectedColumnTitle) {
-        var nameRealColumn;
-        if (selectedColumnTitle == "Car Model") {
-            nameRealColumn = "car_model";
-        }
-        if (selectedColumnTitle == "Driver Name" ) {
-            nameRealColumn = "driver_name";
-        }
-        if (selectedColumnTitle == "Plate Id") {
-            nameRealColumn = "plate_id";
-        }
-        return nameRealColumn;
-    },
-    getColumnId: function(divId, selectedColumnTitle) {
-        var idTitleColumn;
-        var listColumn = document.getElementById(divId).getElementsByClassName("title-column-table");
-        for(var i = 0; i < listColumn.length; i++) {
-            if(listColumn[i].innerText.trimRight() === selectedColumnTitle) {
-                idTitleColumn = listColumn[i].id;
-            }
-        }
-        return idTitleColumn;
-    },
-    getData: function(selectedColumnTitle, callback) {
-        httpRequest(
-            "https://gist.githubusercontent.com/z4y4ts/7170953/raw/7a2b09105b69de8673c4c3acd2b256b83a171dcf/cars.json",
-            function(data) {
-                var columnName = Views.getSelectedColumn(selectedColumnTitle);
-                callback(
-                    Views.sortBy(
-                        JSON.parse(data),
-                        columnName
-                    ),
-                    selectedColumnTitle
-                );
-            }
-        );
-    }
-};
-
+var model;
 var App = {
 	init: function() {
-        Views.createViews();
-        Views.getData("Driver Name", Views.dataView.showData);
+        httpRequest(
+            "https://gist.githubusercontent.com/z4y4ts/7170953/raw/7a2b09105b69de8673c4c3acd2b256b83a171dcf/cars.json",
+            function(data) {
+                model = new viewModel(JSON.parse(data));
+                ko.applyBindings(model);
+            }
+        );
+        //Views.createViews();
+        //sortTableByColumn2();
 	}
 };
 
