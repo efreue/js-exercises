3260d2d4eee255ea720b28a40b2a490de2275e66
efreue <efreue@gmail.com>
2017-01-05 01:43:20 -0300

    esta bastante parecido pero no igual. Falta tipo de letra, y forma de los circulos

--- src/Ejercicio-12/main.js
+++ src/Ejercicio-12/main.js
@@ -11,12 +11,63 @@ var Css = {
 	}
 };
 
+var getCircleColor = function(num, row) {
+    var color = '';
+    if (row == 1) {
+        switch(num) {
+            case 3:
+            case 9:
+            case 12:
+            case 18:
+            case 21:
+            case 27:
+            case 30:
+            case 36:
+                color = 'red';
+                break;
+            default:
+                color = 'black';
+                break;
+        }
+    }
+    else if(row == 2) {
+        switch(num) {
+            case 5:
+            case 14:
+            case 23:
+            case 32:
+                color = 'red';
+                break;
+            default:
+                color = 'black';
+                break;
+        }
+    }
+    else if(row == 3) {
+        switch(num) {
+            case 1:
+            case 7:
+            case 16:
+            case 19:
+            case 25:
+            case 34:
+                color = 'red';
+                break;
+            default:
+                color = 'black';
+                break;
+        }
+    }
+    return color;
+};
+
 var App = {
-    createBall: function(num, color, top) {
+    createCircle: function(num, color) {
         var element = document.createElement('div');
-        Css.add(element, 'ball');
+        Css.add(element, 'circle');
         Css.add(element, color);
-        Css.add(element, top);
+        Css.add(element, 'num-white');
+        Css.add(element, 'horizontal-centered-text');
         element.textContent = num;
         return element;
 
@@ -26,16 +77,22 @@ var App = {
         var table = document.createElement('table');
         var row = document.createElement('tr');
         var th = document.createElement('th');
+        var p = document.createElement('p');
         th.setAttribute('rowspan', 3);
         th.className = "container-board-col0";
+        p.textContent = '0';
+        Css.add(p, 'num-white');
+        Css.add(p, 'prueba');
+        th.appendChild(p);
         row.appendChild(th);
         var numInFirstRow = 0;
         for(var i = 0; i <= 11; i++) {
             numInFirstRow += 3;
             var nodeTh = document.createElement('th');
             nodeTh.className = "container-board-row";
-            var newBallInFirstRow = App.createBall(numInFirstRow, 'red', 'top-first-row');
-            nodeTh.appendChild(newBallInFirstRow);
+
+            var newCircleInFirstRow = App.createCircle(numInFirstRow, getCircleColor(numInFirstRow, 1));
+            nodeTh.appendChild(newCircleInFirstRow);
             row.appendChild(nodeTh);
         }
         table.appendChild(row);
@@ -52,16 +109,16 @@ var App = {
                         numInSecondRow = 2;
                     else
                         numInSecondRow += 3;
-                    var newBallInSecondRow = App.createBall(numInSecondRow, 'red', 'top-second-row');
-                    nodeTrTh.appendChild(newBallInSecondRow);
+                    var newCircleInSecondRow = App.createCircle(numInSecondRow, getCircleColor(numInSecondRow, 2));
+                    nodeTrTh.appendChild(newCircleInSecondRow);
                 }
                 else {
                     if(numInThirdRow === 0)
                         numInThirdRow = 1;
                     else
                         numInThirdRow += 3;
-                    var newBallInThirdRow = App.createBall(numInThirdRow, 'red', 'top-third-row');
-                    nodeTrTh.appendChild(newBallInThirdRow);
+                    var newCircleInThirdRow = App.createCircle(numInThirdRow, getCircleColor(numInThirdRow, 3));
+                    nodeTrTh.appendChild(newCircleInThirdRow);
                 }
                 nodeTr.appendChild(nodeTrTh);
             }
