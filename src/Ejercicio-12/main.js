302d3a4412bf23af3f22c4034525cae1f86bae8e
efreue <efreue@gmail.com>
2017-01-10 00:31:55 -0300

    se corrigio algo de la logica para optimzar el proceso

--- src/Ejercicio-12/main.js
+++ src/Ejercicio-12/main.js
@@ -1,53 +1,42 @@
 getColorForNumber = function(number) {
     var color = '';
-    switch(number) {
-        case 0:
-            color = 'green';
-            break;
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 9:
-        case 12:
-        case 14:
-        case 16:
-        case 18:
-        case 19:
-        case 21:
-        case 23:
-        case 25:
-        case 27:
-        case 30:
-        case 32:
-        case 34:
-        case 36:
-            color = 'red';
-            break;
-        default:
-            color = 'black';
+    if(number == 0) {
+        color = 'green';
     }
+    else if(number % 2 == 0) {
+        if((number <= 10) || (number >= 20 && number <= 28)) {
+            color = 'black';
+        }
+        else {
+            color = 'red';
+        }
+    }
+    else if ((number <= 9) || (number >= 19 && number <= 27)) {
+        color = 'red';
+    }
+    else {
+		color = 'black';
+	}
+
     return color;
 };
-
 
 var getObject = function(name) {
     return document.createElement(name);
 };
 
-var setClassName = function(element, className) {
-    element.className += " " + className;
-}
-
 var getCircle = function(number, callback) {
     var circle = getObject('div');
 
-    setClassName(circle, "shape");
-    setClassName(circle, callback(number));
-    setClassName(circle, "num-white");
-    setClassName(circle, "horizontal-centered-text");
-
+    circle.className = "shape";
+    circle.className += " " + callback(number);
+    circle.className += " num-white";
+    circle.className += " horizontal-centered-text";
     circle.textContent = number;
+
+    if(number == 0) {
+        circle.className += " border-disappear";
+    }
 
     return circle;
 };
@@ -58,31 +47,29 @@ var getCell = function(number, callback) {
     if (number == 0) {
         tableCell.setAttribute('rowspan', 3);
     }
-
     tableCell.className = "container-board";
     tableCell.appendChild(callback);
+
     return tableCell;
 };
 
 var getRow = function() {
-        var row = getObject('tr');
+    var row = getObject('tr');
 
-        row.className = "container-board";
-        row.className = "green";
+    row.className = "container-board";
+    row.className = "green";
 
-        return row;
+    return row;
 };
 
 var getTable = function() {
     var table = getObject('table');
-    table.setAttribute('id', 'board');
     table.className = "container-board";
     return table;
 }
 
 var getRuletteContent = function() {
     var divContent = getObject('div');
-    divContent.setAttribute('id', 'roulette');
     divContent.className = "container-roulette";
     return divContent;
 };
@@ -91,46 +78,36 @@ var showElement = function(element) {
     document.body.appendChild(element);
 };
 
-var loadCell = function(numCell) {
+var createCell = function(numCell) {
     return getCell (
         numCell,
         getCircle(numCell, getColorForNumber)
     )
 };
 
-var loadRow = function(NumberRow, NumberCells) {
+var createRow = function(NumberRow, numberCells) {
     var tableRow = getRow();
+    var increase = 3;
     var cellValue = 0;
-    if (NumberRow == 0) {
+
+    if (NumberRow == 0 ) {
         tableRow.appendChild(
-            loadCell(cellValue)
+            createCell(cellValue)
         )
+        cellValue = 3;
     }
-    for(var i = 0; i <= NumberCells; i++ ) {
-        if (NumberRow == 0) {
-            cellValue += 3;
-        }
-        if ( NumberRow == 1) {
-            if (i == 0) {
-                cellValue += 2;
-            }
-            else {
-                cellValue += 3;
-            }
+    else if (NumberRow == 1) {
+        cellValue = 2;
+    }
+    else {
+        cellValue = 1;
+    }
 
-        }
-        if ( NumberRow == 2) {
-            if (cellValue == 0) {
-                cellValue += 1;
-            }
-            else {
-                cellValue += 3;
-            }
-
-        }
+    for(var i = 0; i <= numberCells; i++ ) {
         tableRow.appendChild(
-            loadCell(cellValue)
+            createCell(cellValue)
         );
+        cellValue += increase;
     }
     return tableRow;
 }
@@ -138,12 +115,14 @@ var loadRow = function(NumberRow, NumberCells) {
 var start = function() {
     var ruletteContent = getRuletteContent();
     var table = getTable();
+
     for (var i = 0; i <= 2; i++) {
         table.appendChild(
-            loadRow(i, 11)
+            createRow(i, 11)
         );
     }
-    ruletteContent.appendChild(table)
+
+    ruletteContent.appendChild(table);
     showElement(ruletteContent);
 };
 
