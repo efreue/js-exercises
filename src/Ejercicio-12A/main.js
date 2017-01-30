593ccf4aab5019932f785646f30b43ee8030c769
efreue <efreue@gmail.com>
2017-01-30 01:49:48 -0300

    se agrego nueva logica

--- src/Ejercicio-12A/main.js
+++ src/Ejercicio-12A/main.js
@@ -1,6 +1,60 @@
+var cleanLowerHierarchy = function(element) {
+     if(element === 'div') {
+        document.getElementById('TableX').value = '';
+        document.getElementById('TableY').value = '';
+        document.getElementById('RowX3').value = '';
+        document.getElementById('RowY3').value = '';
+        document.getElementById('RowX2').value = '';
+        document.getElementById('RowY2').value = '';
+        document.getElementById('RowX1').value = '';
+        document.getElementById('RowY1').value = '';
+     }
+
+     if(element === 'table') {
+        document.getElementById('RowX3').value = '';
+        document.getElementById('RowY3').value = '';
+        document.getElementById('RowX2').value = '';
+        document.getElementById('RowY2').value = '';
+        document.getElementById('RowX1').value = '';
+        document.getElementById('RowY1').value = '';
+     }
+
+    if(element === 'row3') {
+        document.getElementById('RowX2').value = '';
+        document.getElementById('RowY2').value = '';
+        document.getElementById('RowX1').value = '';
+        document.getElementById('RowY1').value = '';
+    }
+
+     if(element === 'row2') {
+        document.getElementById('RowX3').value = '';
+        document.getElementById('RowY3').value = '';
+        document.getElementById('RowX1').value = '';
+        document.getElementById('RowY1').value = '';
+    }
+        if(element === 'row1') {
+        document.getElementById('RowX3').value = '';
+        document.getElementById('RowY3').value = '';
+        document.getElementById('RowX2').value = '';
+        document.getElementById('RowY2').value = '';
+    }
+}
+
+
+var changePositionFicha = function(event) {
+    var ficha = document.getElementById("ficha");
+    ficha.style.left = event.pageX + "px";
+    ficha.style.top = event.pageY + "px";
+
+}
+
 var positionMouse = function(element, event) {
-    var x = event.clientX;
-    var y = event.clientY;
+    var ficha = document.getElementById("ficha");
+    var x = event.pageX - document.getElementById(element).offsetLeft;
+    var y = event.pageY - document.getElementById(element).offsetTop;
+
+    ficha.style.left = event.pageX + "px";
+    ficha.style.top = event.pageY + "px";
 
     if(element === 'div') {
         document.getElementById('DivX').value = x;
@@ -29,6 +83,9 @@ var positionMouse = function(element, event) {
 
 };
 
+
+
+/*
 var dimensionElement = function(element) {
     if(element === 'div') {
         document.getElementById('DivX').value = document.getElementById(element).offsetWidth;
@@ -55,3 +112,4 @@ var dimensionElement = function(element) {
         document.getElementById('RowY1').value = document.getElementById(element).offsetHeight;
     }
 }
+*/
