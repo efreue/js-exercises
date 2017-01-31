94f4be07880a14174114be6e92fc7778c279e806
efreue <efreue@gmail.com>
2017-01-31 08:23:40 -0300

    se agregaron algunos puntos donde estaran las fichas

--- src/Ejercicio-12A/main.js
+++ src/Ejercicio-12A/main.js
@@ -40,21 +40,33 @@ var cleanLowerHierarchy = function(element) {
     }
 }
 
+var createElement = function(name) {
+    return document.createElement(name);
+};
 
-var changePositionFicha = function(event) {
-    var ficha = document.getElementById("ficha");
-    ficha.style.left = event.pageX + "px";
-    ficha.style.top = event.pageY + "px";
+var getCircle = function(id, posLeft, posTop) {
+    var circle = createElement('div');
+    circle.id = id;
+    circle.className = "shape red ";
+    circle.style.left = posLeft + "px";
+    circle.style.top = posTop + "px";
+    return circle;
+};
+
+var addBall = function() {
+    var table = document.getElementById('table');
+    table.appendChild(getCircle(0, 20,150));
+    table.appendChild(getCircle(1, 100,40));
+    table.appendChild(getCircle(2, 175,40));
+    table.appendChild(getCircle(3, 250,40));
+
+    table.appendChild(getCircle(4, 255,90));
+    table.appendChild(getCircle(5, 212,90));
 
 }
-
 var positionMouse = function(element, event) {
-    var ficha = document.getElementById("ficha");
-    var x = event.pageX - document.getElementById(element).offsetLeft;
+   var x = event.pageX - document.getElementById(element).offsetLeft;
     var y = event.pageY - document.getElementById(element).offsetTop;
-
-    ficha.style.left = event.pageX + "px";
-    ficha.style.top = event.pageY + "px";
 
     if(element === 'div') {
         document.getElementById('DivX').value = x;
