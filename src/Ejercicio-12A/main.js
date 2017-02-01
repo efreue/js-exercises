370f1c90a5426720ccc2c95434e8c598e5a79822
efreue <efreue@gmail.com>
2017-02-01 01:13:13 -0300

    se agregaron transparencias y todas las posiciones posibles de las fichas

--- src/Ejercicio-12A/main.js
+++ src/Ejercicio-12A/main.js
@@ -44,10 +44,10 @@ var createElement = function(name) {
     return document.createElement(name);
 };
 
-var getCircle = function(id, posLeft, posTop) {
+var getCircle = function(id, posLeft, posTop, color, transparent, hidden) {
     var circle = createElement('div');
     circle.id = id;
-    circle.className = "shape red ";
+    circle.className = "shape " + transparent + " " + hidden + " " + color;
     circle.style.left = posLeft + "px";
     circle.style.top = posTop + "px";
     return circle;
@@ -55,13 +55,36 @@ var getCircle = function(id, posLeft, posTop) {
 
 var addBall = function() {
     var table = document.getElementById('table');
-    table.appendChild(getCircle(0, 20,150));
-    table.appendChild(getCircle(1, 100,40));
-    table.appendChild(getCircle(2, 175,40));
-    table.appendChild(getCircle(3, 250,40));
+    table.appendChild(getCircle(0, 20, 135, 'red', 'transparent ', ''));
+    table.appendChild(getCircle(1, 100, 40, 'red', '', 'hidden '));
+    table.appendChild(getCircle(2, 175, 40, 'red', 'transparent', ''));
+    table.appendChild(getCircle(3, 250, 40, 'red', '', ''));
 
-    table.appendChild(getCircle(4, 255,90));
-    table.appendChild(getCircle(5, 212,90));
+    table.appendChild(getCircle(4, 100, 90, 'violet', '', ''));
+    table.appendChild(getCircle(5, 135, 90, 'orange', '', ''));
+    table.appendChild(getCircle(6, 175, 90, 'violet', '', ''));
+    table.appendChild(getCircle(7, 215, 90, 'orange', '', ''));
+    table.appendChild(getCircle(8, 250, 90, 'violet', '', ''));
+
+    table.appendChild(getCircle(9, 100, 135, 'red', '', ''));
+    table.appendChild(getCircle(10, 175, 135, 'red', '', ''));
+    table.appendChild(getCircle(11, 250, 135, 'red', '', ''));
+
+    table.appendChild(getCircle(12, 100, 190, 'violet', '', ''));
+    table.appendChild(getCircle(13, 135, 190, 'orange', '', ''));
+    table.appendChild(getCircle(14, 175, 190, 'violet', '', ''));
+    table.appendChild(getCircle(15, 215, 190, 'orange', '', ''));
+    table.appendChild(getCircle(16, 250, 190, 'violet', '', ''));
+
+    table.appendChild(getCircle(17, 100, 235, 'red', '', ''));
+    table.appendChild(getCircle(18, 175, 235, 'red', '', ''));
+    table.appendChild(getCircle(19, 250, 235, 'red', '', ''));
+
+    table.appendChild(getCircle(20, 100, 290, 'green', '', ''));
+    table.appendChild(getCircle(21, 135, 290, 'blue', '', ''));
+    table.appendChild(getCircle(22, 175, 290, 'green', '', ''));
+    table.appendChild(getCircle(23, 215, 290, 'blue', '', ''));
+    table.appendChild(getCircle(24, 250, 290, 'green', '', ''));
 
 }
 var positionMouse = function(element, event) {
