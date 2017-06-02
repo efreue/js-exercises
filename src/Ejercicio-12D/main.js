152cc097d0482ccebc0e46979643099b599dd4f6
efreue <efreue@gmail.com>
2017-06-02 01:02:10 -0300

    se corrigio los nombres de objetos, asi como las observaciones que hizo charly

--- src/Ejercicio-12D/main.js
+++ src/Ejercicio-12D/main.js
@@ -11,10 +11,9 @@ var htmlCreateElement = function(name) {
     return document.createElement(name);
 };
 
-var htmlCreateDiv = function(styleDiv, idDiv) {
+var htmlCreateDiv = function(styleDiv) {
     var divNew = htmlCreateElement('div');
     divNew.className = styleDiv;
-    divNew.id = idDiv;
     return divNew;
 };
 
@@ -44,7 +43,7 @@ var htmlCreateCell = function() {
     var td = htmlCreateElement('td');
     td.className = "container-cell";
     td.onclick = function(e){
-        chip.show(chip.add(e));
+        Chip.show(Chip.add(e));
     };
     return td;
 };
@@ -71,71 +70,68 @@ var getSelectedCell = function(e) {
     };
 }
 
-var board = {
+var Board = {
     chipsByCells: [],
     initialize: function(rows, cols) {
-        board.chipsByCells = [];
+        Board.chipsByCells = [];
         for (var i = 0; i <= rows - 1; i++) {
-            board.chipsByCells[i] = [];
+            Board.chipsByCells[i] = [];
             for (var j = 0; j <= cols - 1; j++) {
-                board.chipsByCells[i][j] = 0;
+                Board.chipsByCells[i][j] = 0;
             }
         }
     },
     getNumberChip: function(rows, cols) {
-        return board.chipsByCells[rows][cols];
+        return Board.chipsByCells[rows][cols];
     },
     setNumberChip: function(rows, cols, num) {
-        board.chipsByCells[rows][cols] = num;
+        Board.chipsByCells[rows][cols] = num;
     }
 };
 
-var chip = {
+var Chip = {
     create: function(row, col, init) {
-        var oneChip = htmlCreateElement('div');
-        oneChip.id = 'chip_id';
-        oneChip.className = "circle";
-        oneChip.onclick = function(e) {
+        var chip = htmlCreateDiv("circle");
+        chip.onclick = function(e) {
             var cellSelected = getSelectedCell(e);
-            var numChip = board.getNumberChip(cellSelected.row, cellSelected.column);
+            var numChip = Board.getNumberChip(cellSelected.row, cellSelected.column);
             if (e.ctrlKey && numChip > 0) {
                 if (((numChip > 1) ? numChip-- : 0) == 0) {
-                    chip.delete(this);
+                    Chip.delete(this);
                     numChip--;
                 }
             }
             else {
                 numChip++;
             }
-            board.setNumberChip(cellSelected.row, cellSelected.column, numChip);
+            Board.setNumberChip(cellSelected.row, cellSelected.column, numChip);
             this.textContent = numChip;
             this.innerHTML = numChip;
         };
-        board.setNumberChip(row, col, init);
-        oneChip.innerHTML = init;
-        oneChip.textContent = init;
-        return oneChip;
+        Board.setNumberChip(row, col, init);
+        chip.textContent = init;
+        return chip;
     },
     add: function(e) {
-        var oneChip = null;
+        var chip = null;
         var cellSelected = getSelectedCell(e);
-        var numChip = board.getNumberChip(cellSelected.row, cellSelected.column);
+        var numChip = Board.getNumberChip(cellSelected.row, cellSelected.column);
         if (numChip === 0 && e.ctrlKey === false) {
             numChip++;
-            oneChip = chip.create(cellSelected.row, cellSelected.column, numChip);
+            chip = Chip.create(cellSelected.row, cellSelected.column, numChip);
         }
         return {
             row: cellSelected.row,
             col: cellSelected.column,
-            oneChip: oneChip
+            chip: chip
         };
     },
     show: function(chipCreated) {
         var marginLeft = (config.cellWidth - config.chipWidth);
         var marginTop = (config.cellHeight - config.chipHeight);
-        chipCreated.oneChip.style.top = (chipCreated.row * config.cellHeight) + marginTop;
-        chipCreated.oneChip.style.left = (chipCreated.col * config.cellWidth) + marginLeft;
-        document.body.appendChild(chipCreated.oneChip);
+        chipCreated.chip.style.top = (chipCreated.row * config.cellHeight) + marginTop;
+        chipCreated.chip.style.left = (chipCreated.col * config.cellWidth) + marginLeft;
+        document.body.appendChild(chipCreated.chip);
     },
     delete: function(chipDel) {
         chipDel.parentElement.removeChild(chipDel);
@@ -146,17 +142,17 @@ var deleteAllChip = function() {
     var chips = document.getElementsByClassName('circle');
     for(var i = chips.length - 1; 0 <= i; i--) {
         if(chips[i] && chips[i].parentElement) {
-            chip.delete(chips[i]);
+            Chip.delete(chips[i]);
         }
     }
-    board.initialize(config.countRows, config.countCols);
+    Board.initialize(config.countRows, config.countCols);
 };
 
 var addNewChip = function() {
     var element = document.getElementsByClassName("label-board");
     var row = parseInt(element[0].value);
     var col = parseInt(element[1].value);
-    var numChip = board.getNumberChip(row, col);
+    var numChip = Board.getNumberChip(row, col);
     numChip++;
     //chip.show(chip.create(row, col, numChip));
 };
@@ -169,15 +165,15 @@ window.addEventListener(
     "load",
     function() {
         var tbl = null;
-        var divContent = htmlCreateDiv("container-div", "divContainerId");
+        var divContent = htmlCreateDiv("container-div");
         tbl = htmlCreateTable(config.countRows, config.countCols);
-        var divDefineCell = htmlCreateDiv("container-show-div", "divResultCell");
+        var divDefineCell = htmlCreateDiv("container-show-div");
         var button = htmlCreateButton("container-button", 'Delete All Chip', deleteAllChip);
         divContent.appendChild(tbl);
         divContent.appendChild(divDefineCell);
         divContent.appendChild(button);
         document.body.appendChild(divContent);
-        board.initialize(config.countRows, config.countCols);
+        Board.initialize(config.countRows, config.countCols);
 
     }
 );
