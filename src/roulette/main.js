8920ea5ff8c085a574dd23b464f144a3679ea1d5
efreue <efreue@gmail.com>
2018-01-12 00:00:11 -0300

    se genero nueva logica

--- src/roulette/main.js
+++ src/roulette/main.js
@@ -1,12 +1,20 @@
 var Config = {
     rows: 3,
-    cols: 13,
+    cols: 12,
     width: 100,
     height: 100,
-    top: 70,
-    left: 30,
-    borderCircle: 3,
-    paddingContainer: 20
+    top: 60,
+    left: 20,
+    borderCircle: 3
+};
+
+var Utils = {
+    getCoordsFromEvent: function(e) {
+        return {
+            x: e.clientX - (Board.element.offsetLeft),
+            y: e.clientY - (Board.element.offsetTop)
+        };
+    }
 };
 
 var Dom = {
@@ -20,184 +28,99 @@ var Dom = {
             element.onclick = clickCallBack;
         }
         return element;
-    }
-};
-
-var Table = {
-    create: function(rows, columns) {
+    },
+    createTable: function(rows, cols, clickCallback){
         var table = Dom.createElement(
-			'table',
-			'hand'
-		);
+            'table',
+            'hand',
+            function(e) {
+                clickCallback(e);
+            }
+        );
         for (var i = rows; i >= 1; i--) {
             table.appendChild(
-                Table.addRow(i, columns - 1)
+                Dom.createRow(i, cols - 1)
             );
         }
         return table;
     },
-    addRow: function(row, col) {
+    createCell: function() {
+        return Dom.createElement(
+            'td',
+            'cells'
+        );
+    },
+    createRow: function(numberRow, numberCells) {
         var tr = Dom.createElement('tr');
-        var number = row;
-        if (row === Config.rows) {
+        if (numberRow == 3) {
             tr.appendChild(
-                Board.addFirstColumn( 0, col, row)
+                Cells.addFirstColumn()
             );
         }
-		row = Config.rows - (row - 1);
-        for (var i = 0; i < col; i++) {
+
+        for (var i = 0; i <= numberCells; i++) {
             tr.appendChild(
-                Table.addCell(number, i, row, false)
+                Cells.add(numberRow)
             );
-            number += 3;
+            numberRow += 3;
         }
         return tr;
-    },
-    addCell: function(number, col, row, isFirstColumn) {
-        var td = Dom.createElement('td', 'cells');
-		var sizeCell = Table.getSizeCell();
-        td.appendChild(
-            Table.getCircle(number, col, row, isFirstColumn, Circle.add)
-        );
-        Table.setSizeCell(td, sizeCell);
+    }
+};
+
+var Cells = {
+    addFirstColumn: function() {
+        var td = Cells.create();
+        td.setAttribute('rowspan', 3);
+        td.appendChild(Circle.add(0));
         return td;
     },
-	getSizeCell: function() {
-		return {
-			width: Config.width,
-			height: Config.height
-		};
-	},
-    setSizeCell: function(element, size) {
-        element.style.width = (size.width + 'px');
-        element.style.height = (size.height + 'px');
+    add: function(numberCell) {
+        var td = Cells.create();
+        td.appendChild(Circle.add(numberCell));
+        return td;
     },
-    getCircle: function(number, col, row, isFirstColumn, callback) {
-        var element = callback(number);
-        var innerElement = {
-            size: Circle.getSize(),
-            border: Circle.getBorder()                        
+    create: function() {
+        var element = Dom.createCell();
+        var sizeCell = Cells.getSize();
+        element.style.width = (sizeCell.width + 'px');
+        element.style.height = (sizeCell.height + 'px');
+        return element;
+    },
+    getSize: function() {
+		return {
+            width: Config.width,
+            height: Config.height
         };
-        var outerElement = {
-           size: Table.getSizeCell()                       
-        }; 
-		var positionCircle = getCenteredPosition(col, row, outerElement, innerElement, isFirstColumn);
-        Circle.setSize(element, innerElement.size);
-        Circle.setPosition(element, positionCircle);
-		return element;
     }
 };
 
 var Circle = {
-	add: function(number) {
-		var circle = Dom.createElement('div');
-		circle.className = "shape num-white " + getColor(number);
-		circle.style.border = (Circle.getBorder() + 'px  solid');
-		circle.textContent = number;
+    add: function(number) {
+        var circle = Dom.createElement('div');
+        var sizeCircle = Circle.getSize();
+
+        circle.className = "shape num-white horizontal-centered-text " + getColor(number);
+        circle.textContent = number;
+    
+        if (number == 0) {
+            circle.style.border = (Config.borderCircle + 'px  solid');
+        }
+        circle.style.width = (sizeCircle.width + 'px');
+        circle.style.height = (sizeCircle.height+ 'px')
         return circle;
-	},
+    },
     getSize: function() {
 		return {
             width: (Config.width - (Config.width / 4)),
             height: (Config.height - (Config.height / 4))
         };
-    },
-	getBorder: function() {
-		return Config.borderCircle;
-	},
-    setSize: function(element, size) {
-        element.style.width = (size.width + 'px');
-		element.style.height = (size.height + 'px');
-
-    },
-    setPosition: function(element, position) {
-		element.style.top = (position.top + 'px');
-		element.style.left = (position.left + 'px');
     }
 };
 
-var Casino = {
-	getPositionChip: function(ev) {
-		var positionBoard = Board.getPosition(ev);
-	}
-};
-
-var HumanPlayer = {
-    bet: function(ev) {
-        //realiza la apuesta
-        //1)obtener la coordenada del evento getCoordFromCells(x, y)
-        //2)obtener la celda getCellFromCoords(x, y)
-        alert('realizar apuesta');
-    },
-    addChip: function(element) {
-		Chip.increment(element);
-	},
-	removeChip: function(element) {
-		Chip.decrement(element);
-	}
-};
-
-var CpuPlayer = {
-    bet: function(ev) {
-        //realiza la apuesta
-        alert('realizar apuesta');
-    },
-    addChip: function(element) {
-		Chip.increment(element);
-	},
-	removeChip: function(element) {
-		Chip.decrement(element);
-	}
-};
-
-var Chip = {
-    create: function() {
-        return {
-            element: Dom.createElement(
-                'div',
-                'chip-circle'
-            ),
-            number: 0
-        };
-    },
-	getSize: function() {
-         return {
-            width: ((Config.width - (Config.width / 4)) / 2),
-            height: ((Config.height - (Config.height / 4)) / 2)
-        };
-    },
-	increment: function(chip) {
-        chip.number++;
-        Chip.update(chip);
-    },
-	decrement: function(chip) {
-        chip.number--;
-        Chip.update(chip);
-    },
-	update: function(chip) {
-        chip.element.textContent = chip.number;
-    }
-};
-
-var getCenteredPosition = function(col, row, outerElement, innerElement, isFirstColumn) {
-	var x;
-    var y;    
-	if (isFirstColumn) {
-		y = ((((row * outerElement.size.height)) / 2) - (innerElement.size.height / 2)) + Config.paddingContainer;
-		x = (outerElement.size.width / 2) - (innerElement.size.width / 2) - innerElement.border + Config.paddingContainer;
-	} else {
-		y = ((row * outerElement.size.height) - (outerElement.size.height / 2) - (innerElement.size.height / 2)) + Config.paddingContainer;
-		x = outerElement.size.width + (((col + 1) * outerElement.size.width) - (outerElement.size.width / 2) - (innerElement.size.width / 2) - innerElement.border) + Config.paddingContainer;
-	}
-	return {
-		left: x,
-		top: y
-	};
-};
-
-var getColor = function (num) {
+var getColor = function(num) {
     var color = '';
-    switch (num) {
+    switch(num) {
         case 0:
             color = 'green';
             break;
@@ -227,61 +150,35 @@ var getColor = function (num) {
     }
     return color;
 };
-/*
-var addEventListener = {
-    getPosition: function(element, typeEvent, callback) {
-        return 
+
+
+var Player = {
+    bettingChip: function(ev) {
+        var positionBoard = Board.getPosition(ev);
     }
 };
-
-*/
-
-var virtualTable = {
-    //alert('agregar logica para poner ficha en posicion donde corresponda');    
-};
-    
 
 var Board = {
-    create: function() {
-        var divContent = Dom.createElement(
-            'div', 
-            'container-div', 
-             HumanPlayer.bet           
+    getPosition: function(ev) {
+        var boardCoords = Board.toBoardCoords(
+            Utils.getCoordsFromEvent(ev)
         );
-        divContent.style.padding = (Board.getPaddingContainer() + 'px');
-        Board.element = Table.create(Config.rows, Config.cols);
-        var chip = Chip.create();
-        chip.element.style.top = '10px';  /*sacar*/
-        chip.element.style.left = '30px'; /*sacar*/
-        chip.element.textContent = '1';  /*sacar*/
-        divContent.appendChild(Board.element);
-        divContent.appendChild(chip.element); /*sacar*/
-        divContent.style.width = ((Config.width * Config.cols) + 'px');
-        divContent.style.height = ((Config.height * Config.rows) + 'px');
-        divContent.style.top = (Config.top + 'px');
-        divContent.style.left = (Config.left + 'px');
-        document.body.appendChild(divContent);
+        return boardCoords;
     },
-    addFirstColumn: function (number, col, row) {
-		var td = Table.addCell(number, col, row, true);
-	    td.setAttribute('rowspan', Config.rows);
-        return td;
-    },
-	getPosition: function(ev) {
-		return Utils.getCoordsFromEvent(ev);
-    },
-    getPaddingContainer: function() {
-		return Config.paddingContainer;
-	}
-};
-
-var Utils = {
-    getCoordsFromEvent: function(e) {
+    toBoardCoords: function(absoluteCoords) {
         return {
-            x: e.clientX - (Board.element.offsetLeft),
-            y: e.clientY - (Board.element.offsetTop)
+            coordCol: absoluteCoords.x,
+            coordRow: absoluteCoords.y
         };
-    }
+    },
+    create: function() {
+        var content = Dom.createElement('div', 'container-board');
+        Board.element = Dom.createTable(Config.rows, Config.cols, Player.bettingChip);
+        content.style.top = (Config.top + 'px');
+        content.style.left = (Config.left + 'px');
+        content.appendChild(Board.element);
+        document.body.appendChild(content);
+    }    
 };
 
 window.addEventListener('load', Board.create);
