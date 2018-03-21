18eeb37272ccbb83e8cbfe537a5f7e64e4df1b49
efreue <efreue@gmail.com>
2018-03-21 00:19:28 -0300

    intengo armar logica para insertar en matriz

--- src/Juego_TaTeTi/main.js
+++ src/Juego_TaTeTi/main.js
@@ -7,8 +7,27 @@ var Cell = {
     getPosition: function(event) {
         var element = event.target;
         var positionElement = element.dataset;
-        console.log('element', positionCell);
+        console.log('element', positionElement);
     }
 };
+/*
+var game = {
+    matriz:[
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ],
+    getMatriz: function() {
+        return this.matriz;
+    },
+    loadMatriz: function(cell) {
+        
+    },
+    start: function() {
+        var play = new this.matriz();
+        play.getMatriz();
+    }
 
+};
+*/
 window.addEventListener('load', StartGame);
