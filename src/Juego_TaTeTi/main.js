d3431b6bed11fb86327b60eb556512f628ca310a
efreue <efreue@gmail.com>
2018-04-12 01:34:25 -0300

    se agrego que al hacer click ingrese botones

--- src/Juego_TaTeTi/main.js
+++ src/Juego_TaTeTi/main.js
@@ -1,21 +1,51 @@
+var Config = {
+    chip_start: 'X'
+};
+
 function StartGame() {
     var Board = document.querySelector('.board');
     var startButton = document.getElementById('startGame');
     startButton.addEventListener('click', game.start);
-    Board.addEventListener('click', Cell.getPosition);
-
+    Board.addEventListener('click', game.play);
+    
+    //console.log(LetterSelected.get());
+    
 }
+
 
 var Cell = {
     getPosition: function(event) {
         var element = event.target;
         var positionElement = element.dataset;
+        //console.log('PositionElement', positionElement.row, positionElement.col);
         if (positionElement && positionElement.row) {
-            console.log('PositionElement', positionElement.row, positionElement.col);
+            return {
+                row: positionElement.row,
+                col: positionElement.col
+            }
         }        
     }
 };
 
+
+var LetterSelected =  {
+    p_Letter: '',    
+    get: function() {
+        return LetterSelected.p_Letter
+    },
+    set: function(letter) {
+        LetterSelected.p_Letter = letter;
+    }
+}; 
+
+
+var Chip = {
+    add: function(cellSelected, letter) {
+        var id = 'cell-' + cellSelected.row + '-' + cellSelected.col;
+        var cellDiv = document.getElementById(id);
+        cellDiv.textContent = letter;
+    }
+};
 
 var game = {
     matriz:[
@@ -25,9 +55,30 @@ var game = {
     ],
     getMatriz: function() {
         return game.matriz;
-    },
+    },    
     start: function() {
         console.log('start');
+    },
+    play: function(event) {
+        var let;
+        if (LetterSelected.get() === '') {
+            LetterSelected.set(Config.chip_start);        
+            let = LetterSelected.get();
+        } 
+        else {
+            if(LetterSelected.get() === 'X') {
+                let = 'O';   
+                LetterSelected.set(let); 
+            }
+            else {
+                let = 'X';   
+                LetterSelected.set(let);     
+            }
+        }
+
+        var cellSel = Cell.getPosition(event);
+        Chip.add(cellSel, let);
+        //console.log('start');
     }
 };
 
