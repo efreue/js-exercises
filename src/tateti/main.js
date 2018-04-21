9e83396f0156c9df886fe6016b0cebf6fb78ed99
efreue <efreue@gmail.com>
2018-04-21 01:54:02 -0300

    Se finalizo el juego - Falta Optimizar Codigo

--- src/tateti/main.js
+++ src/tateti/main.js
@@ -70,25 +70,77 @@ var Chip = {
 };
 
 var Game = {
+    existWin:null,
     play: function(event) {
-        var squareSelected = Square.get(event);
-        if (squareSelected !== null) {
-            var privchip = Chip.create(squareSelected);
-            if (privchip !== null) {
-                Chip.setCount();
-                Game.addMatrix(squareSelected.row, squareSelected.col, privchip.textContent);
+        var squareSelected;
+        if (Game.getWin() === 0 || Game.getWin() === null || Game.getWin() === 'undefined') {
+            squareSelected = Square.get(event);
+            if (squareSelected !== null) {
+                var privchip = Chip.create(squareSelected);
+                if (privchip !== null) {
+                    Chip.setCount();
+                    Game.addMatrix(squareSelected.row, squareSelected.col, privchip.textContent);
+                    if (Chip.getCount() >= 5) {
+                        var symbolWin = Game.getWinners();
+                        if (symbolWin === 'X' || symbolWin === 'O') {
+                            var lblMensaje = document.getElementById('stateGame');
+                            lblMensaje.textContent = 'Win Player ' + symbolWin;     
+                            Game.setWin(1);   
+                        }                         
+                    }
+                }
             }
+        }        
+    },
+    getWinners: function() {
+        if 
+        (
+            ((Matrix[0][0] === Matrix[0][1]) && (Matrix[0][0] === Matrix[0][2])) ||
+            ((Matrix[0][0] === Matrix[1][1]) && (Matrix[0][0] === Matrix[2][2])) ||
+            ((Matrix[0][0] === Matrix[1][0]) && (Matrix[0][0] === Matrix[2][0]))
+        ) {
+            return Matrix[0][0];
+        } else if ((Matrix[0][1] === Matrix[1][1]) && (Matrix[0][1] === Matrix[2][1])) {
+            return Matrix[0][1];
+        } else if ((Matrix[0][2] === Matrix[1][2]) && (Matrix[0][2] === Matrix[2][2])) {
+            return Matrix[0][2];
+        } else if ((Matrix[1][0] === Matrix[1][1]) && (Matrix[1][0] === Matrix[1][2])) {
+            return Matrix[1][0];
+        } else if ((Matrix[2][0] === Matrix[2][1]) && (Matrix[2][0] === Matrix[2][2])) { 
+            return Matrix[2][0];
+        } else if ((Matrix[2][0] === Matrix[1][1]) && (Matrix[2][0] === Matrix[0][2])) { 
+            return Matrix[2][0];
+        } else {
+            return '';
         }
     },
     addMatrix: function(row, col, symbol) {
         Matrix[row][col] = symbol;
     },
     Initialize: function() {
+        var i,j;
+        var lblMensaje = document.getElementById('stateGame');
+        lblMensaje.textContent = '';
+        Game.setWin(0);
+        Chip.increment = 0;
         var buttons = document.getElementsByClassName('cell');
-        for(var i = 0; i < buttons.length; i++) {
+        for(i = 0; i < buttons.length; i++) {
             buttons[i].textContent = '';
         }
         //falta limpiar matrix
+        for (i = 0; i < Matrix.length; i++) {
+            for(j = 0; j < Matrix[i].length; j++) {
+                if (typeof Matrix[i][j] !== 'undefined' && Matrix[i][j] !== null) {
+                    Matrix[i][j] = null;
+                }                
+            }
+        }
+    },
+    setWin: function(value) {
+        Game.existWin = value;
+    },
+    getWin: function() {
+        return Game.existWin;
     } 
 };
 
