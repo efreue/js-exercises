3dedb802fc17ca61f16b5e1f704d666e40bd72bd
efreue <efreue@gmail.com>
2018-05-07 23:52:29 -0300

    se agrego foreach

--- src/Work-With-Matix/main.js
+++ src/Work-With-Matix/main.js
@@ -1,18 +1,17 @@
-var ages = [10,20,11,13,19,18];
+var ages = [10,20,11,13,19,18,41, 15];
 var getLessAge = function(age) {
-  return age <= 18;
+  return age <= 15;
 };
 
 var getAllAge = function(item, index) {
-  var text = '[index: ' + item + '] = ' + item;
-  printResult(text); 
+  printResult('[index: ' + index + ' - VALUE: ' + item + '] '); 
 }
 
 var printResult = function(element) {
-  document.getElementById('result-all').innerText = element + ',';
+  document.getElementById('result-all').innerText += element + ',';
 };
 
 var showResult = function() {
   ages.forEach(getAllAge);
-  document.getElementById('result-less-18').innerText = ages.filter(getLessAge);
+  document.getElementById('result-less-15').innerText = ages.filter(getLessAge);
 };
