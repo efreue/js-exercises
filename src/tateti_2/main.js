31ee868a9f4d8abc424fd7474e423e0eb621ffb5
efreue <efreue@gmail.com>
2018-06-13 00:18:23 -0300

    se logra insertar fichas y cambiar de player. Se uso map, reduce, concat y forEach

--- src/tateti_2/main.js
+++ src/tateti_2/main.js
@@ -3,6 +3,34 @@ var StartGame = function() {
     Board.addEventListener('click', Game.onClickCell);
 };
 
+var Data = {
+    player: 'X',
+    matrix: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ],
+    output: function() {
+        return Data.matrix;
+    },
+    input: function(row, column) {
+        if (Data.setValue(row, column)) {
+            Data.changePlayer();
+        }        
+    },
+    setValue: function(row, column) {
+        var matrix = Data.matrix;
+        if (matrix[row][column] === null) {
+            matrix[row][column] = Data.player;
+            return true;
+        }        
+        return false;
+    },
+    changePlayer: function() {
+        Data.player = Data.player === 'X' ? 'O' : 'X'; 
+    }
+};
+
 var Game = {
     onClickCell: function(event) {
         var target = event.target;
@@ -10,8 +38,40 @@ var Game = {
         //console.log(dataset);
         //console.log('target', target);    
         if (dataset && dataset.row) {
-            console.log('pos', dataset.row, dataset.column);
+            Data.input(dataset.row, dataset.column);
+            Game.play();
         }
+    },
+    play: function() {
+        var data = new Data.output();
+        Game.render(data);
+    },
+    render: function(matrix) {
+        var values = matrix.reduce(
+            function(array, row, rowIndex) {
+                return array.concat(
+                    row.map(
+                        function(cell, cellIndex) {
+                            return {
+                                value: cell,
+                                id: 'cell-' + rowIndex + '-' + cellIndex
+                            };
+                        }
+                    )
+                );
+            },
+            //el valor inicial se pasa vacio
+            []
+        )
+        //console.log(values);
+        values.forEach(function(cell){
+            var cellElement = document.getElementById(cell.id);
+            var valueCell = ''; 
+            if(cell.value != null) {
+                valueCell = cell.value
+            }
+            cellElement.innerHTML = valueCell;
+        });
     }
 };
 window.addEventListener('load', StartGame);
