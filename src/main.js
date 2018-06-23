7a6d55587381efebb933e26582fed9a06282c8bb
efreue <efreue@gmail.com>
2018-06-23 17:40:13 -0300

    se quito porque se agrego mal

--- src/main.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-//lee archivo en forma asincronica
-fs.readFile('partidos.txt','utf8', function(err, data) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log(data.toString());
-});
