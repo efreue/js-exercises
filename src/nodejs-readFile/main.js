10dba61c2c6d2d9c7494036fe1ebdceb5c5d8344
efreue <efreue@gmail.com>
2018-06-25 22:48:37 -0300

    Se finalizo la tarea
    Se Mejoro el codigo, y se agrego setInterval, para verificar cada 5 segundo si hay algun cambio en el archivo partidos.txt

--- src/nodejs-readFile/main.js
+++ src/nodejs-readFile/main.js
@@ -1,8 +1,29 @@
-var fs = require('fs');
-//lee archivo en forma asincronica
-fs.readFile('partidos.txt','utf8', function(err, data) {
-    if (err) {
-        return console.error(err);
+var Document = {
+    contentFile: '',
+    fileName: 'partidos.txt',
+    encoding: 'utf8',
+    readFile: function() {
+        var fs = require('fs');
+        //leo archivo en forma asincronica
+        fs.readFile(
+            Document.fileName,
+            Document.encoding, 
+            function(err, data) {
+                if (err) {
+                    console.error(err);
+                } 
+                else {
+                    if (data.toString() !== Document.contentFile) {
+                        Document.contentFile = data.toString();
+                        console.log(data.toString());                
+                    } 
+                    else {
+                        console.log('archivo no cambio');
+                    }
+                }        
+            }
+        )
     }
-    console.log(data.toString());
-});
+};
+//Cada 5 segundos verifico si cambio el archivo
+setInterval(Document.readFile,5000);
