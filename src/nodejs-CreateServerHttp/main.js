175fe6525d0c0b45727b9fcf3f6fef72433bec31
efreue <efreue@gmail.com>
2018-07-05 23:32:48 -0300

    se muestra en browser un file txt

---
+++ src/nodejs-CreateServerHttp/main.js
@@ -0,0 +1,39 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var File = {
+    encoding: 'utf-8',
+    exists: function(path) {
+        if (fs.existsSync(path)) {
+            return true;
+        } 
+        return false;
+    },
+    getData: function(path) {
+        return fs.readFileSync(path, File.encoding);    
+    }
+}; 
+
+var onRequest = {
+    path: '',
+    start: function(request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        onRequest.path = onRequest.getPath(request.url);
+        if (File.exists(onRequest.path)) {
+            response.write(File.getData(onRequest.path));
+        }
+        else {
+            response.writeHead(404);
+            response.write('File not Found');
+        }
+        response.end();    
+    },
+    getPath: function(urlReq) {
+        return '.' + url.parse(urlReq).pathname;
+    }
+};
+
+var server = http.createServer(onRequest.start);
+server.listen(8000);
+console.log('server started');
