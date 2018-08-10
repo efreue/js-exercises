663472e5f4adee00b648e2bf109316d1d5d9d6a2
efreue <efreue@gmail.com>
2018-08-10 00:19:15 -0300

    se paso a ecma 6

--- src/create-http-server-read-write-file/main.js
+++ src/create-http-server-read-write-file/main.js
@@ -2,32 +2,21 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-var File = {
-    getData: function(path) {
-       return fs.readFileSync(path, 'utf-8');
-    },
-    insertData: function(file, content) {
-       fs.appendFileSync(file, content, 'utf-8');
-    },
-    validateExistsFile: function(path) {
-       return (!fs.existsSync(path)) ? false : true;
-    }
-};
-
 const FileRequest = {
-    fileName: 'partidos.txt',
     start: function(request, response) {
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        var path = FileRequest.getLocation(request.url);
-        if (File.validateExistsFile(path)) {
-            //obtengo parametros del path
-            var urlParams = url.parse(request.url, true).query;
-            if (typeof param.id != 'undefined' || typeof param.team != 'undefined') {
-                var paramText = param.id + ' ' + param.team + "\r\n";
-                File.insertData(path, paramText);
+        const pathLocation = FileRequest.getLocation(request.url);
+        if (fs.existsSync(pathLocation)) {
+            const urlParams = url.parse(request.url, true).query;
+            if (typeof urlParams.id !== 'undefined' || typeof urlParams.team !== 'undefined') {
+                fs.appendFileSync(
+                    pathLocation,
+                    urlParams.id + " " + urlParams.team + "\r\n",
+                    'utf-8'
+                );
             }
-            response.write(File.getData(path));
-        }
+            response.write(fs.readFileSync(pathLocation, 'utf-8'));
+        } 
         else {
             response.writeHead(404);
             response.write('File not Found');
@@ -35,18 +24,10 @@ const FileRequest = {
         response.end();
     },
     getLocation: function(path) {
-        var dir;
-        var currentPath = __dirname;
-        if (url.parse(path).pathname === '/') {
-            dir = currentPath + url.parse(path).pathname + FileRequest.fileName;
-        }
-        else {
-            dir = currentPath + url.parse(path).pathname;
-        }
-        return dir;
+        return __dirname + url.parse(path).pathname + (url.parse(path).pathname == '/' ? 'partidos.txt': '');
     }
 };
 
-var server = http.createServer(FileRequest.start);
+const server = http.createServer(FileRequest.start);
 server.listen(8000);
 console.log('server started');
