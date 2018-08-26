9ab64ad2217541f82f376cdd4d963bb5c09b8cb7
efreue <efreue@gmail.com>
2018-08-26 12:08:56 -0300

    Se importaron parte de los package url, js y http

--- src/create-http-server-read-write-file/main.js
+++ src/create-http-server-read-write-file/main.js
@@ -1,22 +1,22 @@
 //import {createServer as httpCreateServer} from "http.js";
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const {createServer} = require('http');
+const {existsSync, appendFileSync, readFileSync} = require('fs');
+const {parse} = require('url');
 
 const FileRequest = {
     start: (request, response) => {
         response.writeHead(200, {'Content-Type': 'text/plain'});
         const pathLocation = FileRequest.getLocation(request.url);
-        if (fs.existsSync(pathLocation)) {
+        if (existsSync(pathLocation)) {
             const urlParams = url.parse(request.url, true).query;
             if (typeof urlParams.id !== 'undefined' || typeof urlParams.team !== 'undefined') {
-                fs.appendFileSync(
+                appendFileSync(
                     pathLocation,
                     urlParams.id + " " + urlParams.team + "\r\n",
                     'utf-8'
                 );
             }
-            response.write(fs.readFileSync(pathLocation, 'utf-8'));
+            response.write(readFileSync(pathLocation, 'utf-8'));
         }
         else {
             response.writeHead(404);
@@ -25,9 +25,9 @@ const FileRequest = {
         response.end();
     },
     getLocation: (path) => {
-        return __dirname + url.parse(path).pathname + (url.parse(path).pathname == '/' ? 'partidos.txt' : '');
+        return __dirname + parse(path).pathname + (parse(path).pathname == '/' ? 'partidos.txt' : '');
     }
 };
 
-http.createServer(FileRequest.start).listen(8000);
+createServer(FileRequest.start).listen(8000);
 console.log('server started');
