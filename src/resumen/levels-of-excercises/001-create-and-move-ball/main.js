7391f0dbf8e981865eabb13b972b643f4e40c789
Emiliano Freue <efreue@admotion.local>
2019-01-08 19:47:22 -0300

    la bola comenzo a moverse en forma horizontal

--- src/resumen/levels-of-excercises/001-create-and-move-ball/main.js
+++ src/resumen/levels-of-excercises/001-create-and-move-ball/main.js
@@ -1,7 +1,7 @@
 var Config = {
-    step: 24
+    step: 24,
+    interval: 80
 };
-
 
 var Css = {
     add: function(node, className) {
@@ -25,37 +25,108 @@ var Ball = {
     create: function() {
         var elementShowBall = document.getElementById("spaceShowBall");
         var ball = Element.create("div", "ball","");
+        ball.directionX = "left";
         elementShowBall.appendChild(ball);
         App.allBalls.push(ball);
+    },
+    moveY: function(element) {
+        if (App.started === 1) {
+            var content = document.getElementById('parentBoard');
+            var screenSizeX = content.clientWidth - element.clientWidth;
+            var positionX = element.offsetLeft;
+            if ((positionX <= screenSizeX) && (element.directionX == "left")) {
+                element.directionX = "left";
+                positionX += Config.step;
+                element.style.left = positionX + 'px';
+            } else {
+                element.directionX = "right"
+                positionX -= Config.step;
+                if (positionX >= 0) {
+                    element.style.left = positionX + 'px';
+                }
+                else {
+                    element.directionX = "left";
+                }
+            }
+        }
+        
+    }
+};
+
+var Button = {
+    add: function() {
+        document.getElementById('btnPlay').disabled = false;
+        document.getElementById('btnPause').disabled = true;
+        Ball.create();
+    },
+    play: function() {
+        document.getElementById('btnPlay').disabled = true;
+        document.getElementById('btnPause').disabled = false;
+        App.startBall();
+        //alert('click play');
+    },
+    pause: function() {
+        document.getElementById('btnPlay').disabled = false;
+        document.getElementById('btnPause').disabled = true;
+        App.started = 0;
+        alert('click pause');
     }
 };
 
 var App = {
+    started: 0,
     allBalls:[],
     inicialize: function() {
         var grandParentBoard = Element.create("div", "grandParentBoard", "");
         var contentButton = Element.create("div", "parentFooter", "");
         var parentBoard = Element.create("div", "parentBoard", "");
         var childBoard = Element.create("div", "childBoard", "");
-        
-        childBoard.setAttribute("id", "spaceShowBall");
+        var btnAdd = Element.create(
+            "button", 
+            "buttons", 
+            "Add",
+            function(e) {
+                Button.add();
+            }
+        );
+        var btnPause = Element.create(
+            "button", 
+            "buttons", 
+            "Pause",
+            function(e) {
+                Button.pause();
+            }
+        );
+        var btnPlay = Element.create(
+            "button", 
+            "buttons", 
+            "Play",
+            function(e) {
+                Button.play();
+            }
+        );
+        parentBoard.setAttribute("id", 'parentBoard');
+        btnAdd.setAttribute("id", 'btnAdd');
+        btnPause.setAttribute("id", 'btnPause');
+        btnPlay.setAttribute("id", 'btnPlay');
+        childBoard.setAttribute("id", 'spaceShowBall');
         grandParentBoard.appendChild(parentBoard);
         grandParentBoard.appendChild(contentButton);
         parentBoard.appendChild(childBoard);
-        contentButton.appendChild(
-            Element.create(
-                "button", 
-                "buttons", 
-                "Add",
-                function(e) {
-                    Ball.create();
-                }
-            )
-        );
-        contentButton.appendChild(Element.create("button", "buttons", "Pause"));
-        contentButton.appendChild(Element.create("button", "buttons", "Play"))
+        contentButton.appendChild(btnAdd);
+        contentButton.appendChild(btnPause);
+        contentButton.appendChild(btnPlay);
         document.body.appendChild(grandParentBoard);
-    } 
+    },
+    startBall: function() {
+        App.started = 1;
+        var id = setInterval(App.moveBall, Config.interval);
+    },
+    moveBall: function() {
+        for(var i = 0; i < App.allBalls.length; i++) {
+            Ball.moveY(App.allBalls[i]);
+        }
+    }
 };
 
 window.addEventListener('load', App.inicialize);
