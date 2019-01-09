83389fe76cca180764ed43880eb24b2504416575
Emiliano Freue <efreue@admotion.local>
2019-01-09 19:16:30 -0300

    analizar main.js es6, no funciona por css

--- src/resumen/levels-of-excercises/001-create-and-move-ball-ES6/main.js
+++ src/resumen/levels-of-excercises/001-create-and-move-ball-ES6/main.js
@@ -17,6 +17,14 @@ var Element = {
             elem.onclick = callback;
         }
         return elem;
+    },
+    disable(obj) {
+        obj.disabled = true;
+        Css.add(obj, 'disabled');
+    },
+    enable(obj) {
+        obj.disabled = false;
+        obj.className.remove('disabled');
     }
 };
 
@@ -77,6 +85,80 @@ var Ball = {
 
 var Button = {
     add() {
-        
+        Element.enable(document.getElementById('btnPlay'));
+        Element.enable(document.getElementById('btnPause'));
+        Ball.create(document.getElementById('spaceShowBall'));
+    },
+    play() {
+        Element.enable(document.getElementById('btnadd'));
+        Element.disable(document.getElementById('btnPlay'));
+        Element.enable(document.getElementById('btnPause'));
+        App.startBall();
+    },
+    pause() {
+        Element.disable(document.getElementById('btnadd'));
+        Element.enable(document.getElementById('btnPlay'));
+        Element.disable(document.getElementById('btnPause'));
+        App.started = 0;
     }
 }
+
+var App = {
+    started: 0,
+    allBalls: [],
+    inicialize() {
+        let grandParentBoard = Element.create('div', 'grandParentBoard', '');
+        let contentButton = Element.create('div', 'parentFooter', '');
+        let parentBoard = Element.create('div', 'parentBoard', '');
+        let childBoard = Element.create('div', 'childBoard', '');
+        let btnAdd = Element.create(
+            'button',
+            'buttons',
+            'Add',
+            (e) => {
+                Button.add();
+            }
+        );
+        let btnPause = Element.create(
+            'button',
+            'buttons',
+            'Pause',
+            (e) => {
+                Button.pause();
+            }
+        );
+        let btnPlay = Element.create(
+            'button',
+            'buttons',
+            'Play',
+            (e) => {
+                Button.play();
+            }
+        );
+        parentBoard.setAttribute('id', 'parentBoard');
+        btnAdd.setAttribute('id', 'btnAdd');
+        btnPause.setAttribute('id', 'btnPause');
+        btnPlay.setAttribute('id', 'btnPlay');
+        childBoard.setAttribute('id', 'spaceShowBall');
+        grandParentBoard.appendChild(parentBoard);
+        grandParentBoard.appendChild(contentButton);
+        parentBoard.appendChild(childBoard);
+        contentButton.appendChild(btnAdd);
+        contentButton.appendChild(btnPause);
+        contentButton.appendChild(btnPlay);
+        document.body.appendChild(grandParentBoard);
+    },
+    startBall() {
+        App.started = 1;
+        let id = setInterval(App.moveBall, Config.interval);
+        if (id > 1) {
+            clearInterval(id);
+        }            
+    },
+    moveBall() {
+        App.allBalls.forEach((index) => {
+            Ball.move(App.allBalls[index])
+        }); 
+    }
+};
+window.addEventListener('load', App.inicialize);
