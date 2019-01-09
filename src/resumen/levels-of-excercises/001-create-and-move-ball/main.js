3e831be02dd772cc1a44002338b3ea81be9e3cde
Emiliano Freue <efreue@admotion.local>
2019-01-09 12:29:50 -0300

    Se finalizo el desarrollo

--- src/resumen/levels-of-excercises/001-create-and-move-ball/main.js
+++ src/resumen/levels-of-excercises/001-create-and-move-ball/main.js
@@ -23,33 +23,65 @@ var Element = {
 
 var Ball = {
     create: function() {
-        var elementShowBall = document.getElementById("spaceShowBall");
+        var content = document.getElementById("spaceShowBall");
         var ball = Element.create("div", "ball","");
         ball.directionX = "left";
-        elementShowBall.appendChild(ball);
+        ball.directionY = "down";
+        content.appendChild(ball);
         App.allBalls.push(ball);
     },
-    moveY: function(element) {
-        if (App.started === 1) {
-            var content = document.getElementById('parentBoard');
-            var screenSizeX = content.clientWidth - element.clientWidth;
-            var positionX = element.offsetLeft;
-            if ((positionX <= screenSizeX) && (element.directionX == "left")) {
-                element.directionX = "left";
-                positionX += Config.step;
-                element.style.left = positionX + 'px';
+    changePositionX: function(ball, dirX, positionX) {
+        ball.directionX = dirX;
+        if (dirX == "left") {
+            ball.style.left = positionX + 'px';
+        } else {
+            if (positionX >= 0) {
+                ball.style.left = positionX + 'px';
             } else {
-                element.directionX = "right"
-                positionX -= Config.step;
-                if (positionX >= 0) {
-                    element.style.left = positionX + 'px';
-                }
-                else {
-                    element.directionX = "left";
-                }
+                ball.directionX = "left";
             }
         }
-        
+    }, 
+    changePositionY: function(ball, dirY, positionY) {
+        ball.directionY = dirY;
+        if (dirY == "down") {
+            ball.style.top = positionY + 'px';
+        } else {
+            if (positionY >= 0) {
+                ball.style.top = positionY + 'px';
+            } else {
+                ball.directionY = "down";
+            }
+        }
+    }, 
+    moveX: function(ball, elementParent) {
+        var screenSizeX = elementParent.clientWidth - ball.clientWidth - 40;
+        var positionX = ball.offsetLeft;
+        if ((positionX <= screenSizeX) && (ball.directionX == "left")) {
+            positionX += Config.step;
+            Ball.changePositionX(ball, "left", positionX);            
+        } else {
+            positionX -= Config.step;
+            Ball.changePositionX(ball, "right", positionX);                        
+        }
+    },
+    moveY: function(ball, elementParent) {
+        var screenSizeY = elementParent.clientHeight - ball.clientHeight - 40;
+        var positionY = ball.offsetTop;
+        if ((positionY <= screenSizeY) && (ball.directionY == "down")) {
+            positionY += Config.step;
+            Ball.changePositionY(ball, "down", positionY);
+        } else {
+            positionY -= Config.step;
+            Ball.changePositionY(ball, "top", positionY);
+        }
+    },
+    move: function(ball) {
+        if (App.started === 1) {
+            var content = document.getElementById('parentBoard');
+            Ball.moveX(ball, content);
+            Ball.moveY(ball, content);
+        }
     }
 };
 
@@ -121,12 +153,14 @@ var App = {
     startBall: function() {
         App.started = 1;
         var id = setInterval(App.moveBall, Config.interval);
+        if (id > 1) {
+            clearInterval(id);
+        }            
     },
     moveBall: function() {
         for(var i = 0; i < App.allBalls.length; i++) {
-            Ball.moveY(App.allBalls[i]);
+            Ball.move(App.allBalls[i]);
         }
     }
 };
-
 window.addEventListener('load', App.inicialize);
