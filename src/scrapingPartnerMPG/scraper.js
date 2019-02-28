f39a6dea2acdfe5ce33163affcacaeebbfbec6ce
efreue <efreue@gmail.com>
2019-02-28 00:01:47 -0300

    se finalizo ejemplo de paginacion

--- src/scrapingPartnerMPG/scraper.js
+++ src/scrapingPartnerMPG/scraper.js
@@ -1,17 +1,38 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const url =
-    "https://marketingplatform.google.com/about/partners/find-a-partner?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fintl%2Fes%2Fabout%2F";
-  await page.goto(url);
-  //await page.screenshot({ path: "example.png" });
+  const extractPartners = async (url) => {
+    const page = await browser.newPage();
+    await page.goto(url);
+    console.log(`scraping: ${url}`);
 
-  const titles = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("div.compact h3.title"))
-        .map(partner => partner.innerText.trim())
+    // Scrape the data we want
+    const partnersOnPage = await page.evaluate(() =>
+      Array.from(document.querySelectorAll("div.compact"))
+      .map(compact => ({
+          title: compact.querySelector('h3.title').innerText.trim(),
+          logo: compact.querySelector('.logo img').src		
+        })
+      )
     );
-  console.log(titles);
-  await browser.close();
+   
+    await page.close();
+    
+    // Should we end recursion?
+    if(partnersOnPage.length < 1) {
+      console.log(`Terminate recursion on: ${url}`);
+      return partnersOnPage
+    } else {
+      // Go fetch de next page
+      const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) + 1;
+      const nextUrl = `https://marketingplatform.google.com/about/partners/find-a-partner?page=${nextPageNumber}`;
+      return partnersOnPage.concat(await extractPartners(nextUrl))
+    }   
+  };
+  const browser = await puppeteer.launch();
+  const firstUrl =
+    "https://marketingplatform.google.com/about/partners/find-a-partner?page=45";
+  const partners = await extractPartners(firstUrl);
+  console.log(partners);
+  
 })();
